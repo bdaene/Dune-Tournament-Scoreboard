1b3a18b7c09764e89d42881580915ae3f5b8d87d
cbroeckx <broeckx.christophe@gmail.com>
2024-01-11 19:45:33 +0100

    Cleanup

--- dune_tournament_scoreboard/gui/tournaments_selection.py
+++ dune_tournament_scoreboard/gui/tournaments_selection.py
@@ -6,34 +6,35 @@ from dune_tournament_scoreboard.gui.utils import *
 
 
 class TournamentSelection(ctk.CTkToplevel):
-    def __init__(self, master, display, **kwargs):
+    def __init__(self, master, display_app, **kwargs):
         super().__init__(master, **kwargs)
         self.title("Sélection du tournoi")
 
-        def select_tournament(choice):
-            tournament.select(choice)
-            display()
-            self.destroy()
-            self.update()
-
-        def create_tournament():
-            dialog = ctk.CTkInputDialog(text="Choisissez un nom de tournoi", title="Créer un tournoi")
-            get_input = dialog.get_input()
-            tournament.create(get_input)
-            select_tournament(get_input)
+        # Save display function
+        self.display_app = display_app
 
         # New tournament
-        tournament_creation_button = ctk.CTkButton(self, text="Créer un nouveau tournoi",
-                                                   command=create_tournament)
+        tournament_creation_button = ctk.CTkButton(self, text="Créer un nouveau tournoi", command=_create_tournament)
         tournament_creation_button.grid(row=0, column=0, padx=10, pady=10)
 
         # Existing tournaments
         tournaments = [element.replace('_', ' ') for element in tournament.list_tournaments()]
         if tournaments:
             selected_tournament = ctk.StringVar(value="Choisissez un tournoi existant")
-            available_tournaments = ctk.CTkOptionMenu(self, values=tournaments,
-                                                      command=select_tournament,
+            available_tournaments = ctk.CTkOptionMenu(self, values=tournaments, command=_select_tournament,
                                                       variable=selected_tournament)
             available_tournaments.grid(row=0, column=1, padx=10, pady=10)
 
         center_frame(self)
+
+    def _select_tournament(self, choice):
+        tournament.select(choice)
+        self.display_app()
+        self.destroy()
+        self.update()
+
+    def _create_tournament(self):
+        dialog = ctk.CTkInputDialog(text="Choisissez un nom de tournoi", title="Créer un tournoi")
+        get_input = dialog.get_input()
+        tournament.create(get_input)
+        self._select_tournament(get_input)
